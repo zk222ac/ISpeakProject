57d3388f3eeeb216f18b8a50a18fb713c06a4e2f
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-02-03 21:11:22 +0100

    hi

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -10,6 +10,7 @@ namespace Sample
     {
         static void Main(string[] args)
         {
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
